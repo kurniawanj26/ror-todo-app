8a9e7cab5777e09102e9fbb79dc716f286beb09f
“kurniawanj26” <jayadi.kurniawan@gmail.com>
2025-04-08 01:47:40 +0700

    integrate delete API
    - FEAT : integrate delete API
    - FEAT : add delete function

--- frontend/src/api/endpoints.js
+++ frontend/src/api/endpoints.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const BASE_URL = "http://localhost:3000/api/";
 const GET_URL = `${BASE_URL}todos`;
 const POST_URL = `${BASE_URL}todos`;
+const DELETE_URL = (id) => `${BASE_URL}todos/${id}`;
 
 export const get_todos = async () => {
   const response = await axios.get(GET_URL);
@@ -14,5 +15,10 @@ export const add_todo = async (todo_name) => {
     todo_name: todo_name,
     completed: false,
   });
+  return response.data;
+};
+
+export const delete_todo = async (id) => {
+  const response = await axios.delete(DELETE_URL(id));
   return response.data;
 };
--- frontend/src/components/Todo.js
+++ frontend/src/components/Todo.js
@@ -1,12 +1,16 @@
 import { RiDeleteBack2Line } from "react-icons/ri";
 
-const Todo = ({ todo_name }) => {
+const Todo = ({ todo_name, id, deleteTodo }) => {
+  const handleDelete = () => {
+    deleteTodo(id);
+  };
+
   return (
     <div className="todo">
       <div className="todo-container">
         <input type="checkbox" />
         <h3>{todo_name}</h3>
-        <RiDeleteBack2Line size="20px" />
+        <RiDeleteBack2Line size="20px" onClick={handleDelete} />
       </div>
     </div>
   );
